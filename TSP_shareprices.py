ac1f7f6e2f299c492e8a0eb87f1b49a756d51f03
CyrusArdain <michael.magyar@gmail.com>
2015-02-28 18:42:44 -0500

    update

--- TSP_shareprices.py
+++ TSP_shareprices.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        TSP_shareprices
 # Author:      musashi
 # Created:     28/02/2015
 # Copyright:   (c) musashi 2015
@@ -8,19 +8,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+#TSP Shareprice website and input data
 url = 'https://www.tsp.gov/investmentfunds/shareprice/sharePriceHistory.shtml'
 post_data = {"startdate" : "02/25/2015",  "enddate":	"02/27/2015"}
 
+#Submits POST data to website and gets website output
 response = requests.post(url, data = post_data)
 
+#Converts 'response' text to beautifulsoup format
 soup = BeautifulSoup(response.text)
 
+#Extracts the dates and shareprices
 dates = soup.findAll(name='td', attrs={"class":"leadingCell"})
 price = soup.findAll(name="td",attrs={"class": "packed"}, limit=10)
 
-with open('test2.csv', 'a') as f:
+
+#Writes to .csv file
+with open('TSP_shareprices.csv', 'wb') as f:
+
+    #Loop for writing each date
     for count in range (0, len(dates)):
+
             #Converts stored data to ascii format, cleans up format to be .csv compliant
             date = dates[count].contents[0].encode('ascii')
             date = date.replace(',', '')
